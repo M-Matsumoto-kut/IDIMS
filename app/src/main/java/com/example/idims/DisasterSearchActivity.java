459c3f50a387ba4db4ffae9c267ae191657c052a
M-Matsumoto-kut <socialmasa@gmail.com>
2022-01-13 22:55:00 +0900

    災害検索アクティビティの追加

---
+++ app/src/main/java/com/example/idims/DisasterSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.idims;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisasterSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disaster_search);
+    }
+}
