1b6a9497ae6e58fb3c4279ea1609c397ee22ea8b
TMahiro <t1m31018@gmail.com>
2022-01-14 14:18:16 +0900

    いろいろいじった

--- app/src/main/java/com/example/idims/Login/FirstFragment.java
+++ app/src/main/java/com/example/idims/Login/FirstFragment.java
@@ -1,4 +1,4 @@
-package com.example.idims;
+package com.example.idims.Login;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.idims.R;
 import com.example.idims.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
