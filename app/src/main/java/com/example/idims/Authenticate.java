19574e997673bdefd006c057eea8ae49ce13eb4e
TMahiro <t1m31018@gmail.com>
2022-01-15 13:40:18 +0900

    Authenticate（ID/パスワード，現在・新しいパスワードの認証を行うクラス）追加

---
+++ app/src/main/java/com/example/idims/Authenticate.java
@@ -0,0 +1,30 @@
+package com.example.idims;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Authenticate extends AppCompatActivity {
+
+    //IDとパスワードが一致しているか検証(1：一致，2:不一致)
+    public static boolean loginAuthenticate(String id, String password) {
+        boolean match = false;
+
+
+        return match;
+    }
+
+    //現在のパスワードが一致しているか検証(1：一致，2:不一致)
+    public static boolean nowPasswordAuthenticate(String nowPassword) {
+        boolean match = false;
+
+
+        return match;
+    }
+
+    //新しいパスワードが規定通りか検証(1：一致，2:不一致)
+    public static boolean newPasswordAuthenticate(String newPassword) {
+        boolean match = false;
+
+
+        return match;
+    }
+}
