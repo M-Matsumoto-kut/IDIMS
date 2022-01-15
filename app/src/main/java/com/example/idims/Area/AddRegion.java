65249aa9c054c07340526d37ab3d24f24f78df96
TMahiro <t1m31018@gmail.com>
2022-01-16 02:06:16 +0900

    閲覧地域一覧(AddRegionクラス），閲覧地域追加(AddRegionクラス)を作成

---
+++ app/src/main/java/com/example/idims/Area/AddRegion.java
@@ -0,0 +1,20 @@
+package com.example.idims.Area;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddRegion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+}
