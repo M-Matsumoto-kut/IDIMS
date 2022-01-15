f0f651b5ce044af0cad3d551b08c1d1e17c227b1
M-Matsumoto-kut <socialmasa@gmail.com>
2022-01-16 01:29:05 +0900

    災害検索画面内の地域選択画面アクティビティの追加とそれに関連する処理の追加

--- app/src/main/java/com/example/idims/DisasterSearchActivity.java
+++ app/src/main/java/com/example/idims/DisasterSearchActivity.java
@@ -2,13 +2,34 @@ package com.example.idims;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+//災害検索アクティビティ
 public class DisasterSearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //画面の表示
         setContentView(R.layout.activity_disaster_search);
+
+        //検索地域選択ボタン押下時の処理
+        Button serectAreaButton = (Button) findViewById(R.id.button_SelectArea);
+        serectAreaButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(DisasterSearchActivity.this, )
+            }
+        });
+
+        //検索結果表示ボタン押下時の処理
+        Button resultButton = (Button) findViewById(R.id.button_Search);
+        resultButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+
+            }
+        });
     }
 }
