cb63142eb8f875af7c9a1d5d518d05fca7a652d0
TMahiro <t1m31018@gmail.com>
2022-01-15 20:35:18 +0900

    MainActivityと新たにStatusFlag（画面やユーザタイプの情報を記録し，必要時に値を返すクラス．javaファイル参照）を追加

---
+++ app/src/main/java/com/example/idims/StatusFlag.java
@@ -0,0 +1,69 @@
+package com.example.idims;
+
+import android.app.Application;
+
+public class StatusFlag extends Application {
+    private int activityStatus;     // 起動時のActivityの状態
+
+    private int loginType;       // login時のユーザのタイプ
+    private int id;              // (研究者)ログイン後IDを記録する
+    private String password;        // (研究者)ログイン後パスワードを記録する
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.setActivityStatus(1);
+    }
+
+    /*
+        activityStatusの値を返す
+        1:StartLogin
+        2:
+        3:
+     */
+    public int getActivityStatus(){
+        return this.activityStatus;
+    }
+
+    //Activityの状態を更新
+    public void setActivityStatus(int status) {
+        this.activityStatus = status;
+    }
+
+    /*
+        loginTypeの値を返す
+        1:generalUser
+        2:researcherUser
+     */
+    public int getLoginType() {
+        return this.loginType;
+    }
+
+    //loginTypeの値を更新
+    public void setLoginType(int status) {
+        this.loginType = status;
+    }
+
+    //idを返す
+    public int getId() {
+        return this.id;
+    }
+
+    //idの値を更新
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //passwordを返す
+    public String getPassword() {
+        return this.password;
+    }
+
+    //passwordの値を更新
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
