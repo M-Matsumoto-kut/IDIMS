849cad0fc4fac8ade129c0bed2dde14645e2c20e
M-Matsumoto-kut <socialmasa@gmail.com>
2022-01-16 03:54:16 +0900

    検索条件を格納するクラスの作成と、災害種類のチェックボックスの動作関連の追加

--- app/src/main/java/com/example/idims/DisasterSearchActivity.java
+++ app/src/main/java/com/example/idims/DisasterSearchActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,11 +17,17 @@ import java.sql.Statement;
 //災害検索アクティビティ
 public class DisasterSearchActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //画面の表示
         setContentView(R.layout.activity_disaster_search);
+
+        //検索条件を格納するSearchConditionsの宣言
+        SearchConditions search = new SearchConditions();
 
         //検索地域選択ボタン押下時の処理
         Button serectAreaButton = (Button) findViewById(R.id.button_SelectArea);
@@ -31,24 +38,25 @@ public class DisasterSearchActivity extends AppCompatActivity {
             }
         });
 
+        //災害の種類のチェックボタンの定義(上から津波、土砂崩れ、雷)
+        CheckBox checkBox_Wave = (CheckBox) findViewById(R.id.checkBox_Wave);
+        CheckBox checkBox_Landsride = (CheckBox) findViewById(R.id.checkBox_Landsride);
+        CheckBox checkBox_thounder = (CheckBox) findViewById(R.id.checkBox_Thounder);
+        //種類チェックボタンが押されているかを格納するboolean型
+        boolean wave_On = false;
+        boolean landsride_On = false;
+        boolean thounder = false;
+        //それぞれのチェックボタンが
+
         //検索結果表示ボタン押下時の処理
         Button resultButton = (Button) findViewById(R.id.button_Search);
         resultButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                Connection con_SearchResult = null;
+                //SearchCondisionsクラスに検索条件を引き渡す
 
-                //try文でデータベースに接続し、失敗した場合はSQL例外で処理終了
-                try{
-                    //データベースMySQLに接続
-                    con_SearchResult = DriverManager.getConnection(uri, username, passward);
-                    Statement statement = con_SearchResult.createStatement();
-
-                } catch(SQLException e){
-
-                }
-
-                //検索結果画面を表示
-                //Intent intent = new Intent(DisasterSearchActivity.this, );
+                //検索結果画面へ移動
+                Intent intent = new Intent(DisasterSearchActivity.this, );
+                startActivity(intent);
 
             }
         });
