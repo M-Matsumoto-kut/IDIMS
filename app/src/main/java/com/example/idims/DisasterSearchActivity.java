7fbfaa0f2637be881618da48436374c6df562463
M-Matsumoto-kut <socialmasa@gmail.com>
2022-01-16 19:49:54 +0900

    検索条件クラスの完成

--- app/src/main/java/com/example/idims/DisasterSearchActivity.java
+++ app/src/main/java/com/example/idims/DisasterSearchActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.ArraySet;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,12 +20,16 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 //災害検索アクティビティ
 public class DisasterSearchActivity extends AppCompatActivity {
 
-    int areaNum = 0;
-    static final int RESULT_AREA = 1000;
+    int areaNum = 0; //地方を選択する
+    int disasterCount = 0; //災害の数が選択されている数を表す
+    static final int RESULT_AREA = 1000; //getexistを使用するのに必要
+
+    public static final String SEARCHCONDITIONS_DATA = "com.example.idims.SearchConditions";//検索結果アクティビティに検索条件を引き渡すのに必要
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,8 +56,10 @@ public class DisasterSearchActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 if(isChecked) {
                     search.setWave_on(true);
+                    disasterCount++;
                 }else{
                     search.setWave_on(false);
+                    disasterCount--;
                 }
             }
         });
@@ -63,8 +70,10 @@ public class DisasterSearchActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 if(isChecked) {
                     search.setLandsride_On(true);
+                    disasterCount++;
                 }else{
                     search.setLandsride_On(false);
+                    disasterCount--;
                 }
             }
         });
@@ -75,8 +84,10 @@ public class DisasterSearchActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 if(isChecked) {
                     search.setThounder_On(true);
+                    disasterCount++;
                 }else{
                     search.setThounder_On(false);
+                    disasterCount--;
                 }
             }
         });
@@ -138,24 +149,32 @@ public class DisasterSearchActivity extends AppCompatActivity {
             public void onClick(View view){
                 //検索条件を満たさない場合エラーを起こす
                 //地域が選択されていない場合
-                if(areaNum == 0){
-                    TextView textView = (TextView) findViewById(R.id.textView_Error);
-                    textView.setText("地域を選択してください");
-                    break;
+                switch(areaNum){
+                    case 0:
+                        TextView textView = (TextView) findViewById(R.id.textView_Error);
+                        textView.setText("地域を選択してください");
+                        break;
                 }
-
                 //災害の種類が一つも選択されていない場合
-                //自由設定期間が正しく設定されていない場合
-                //検索結果画面へ移動
-                Connection con = null;
-                try{
-                    con = DriverManager.getConnection(jdbc:mysql://);
-
-                    Statement state = con.createStatement();
-                    String sql =
+                switch(disasterCount){
+                    case 0:
+                        TextView textView = (TextView) findViewById(R.id.textView_Error);
+                        textView.setText("災害を1種類以上選んでください");
+                        break;
                 }
+
+                //自由設定期間が正しく設定されていない場合
+                switch(overTime(search.startDate, search.endDate)){
+                    case 0:
+                        TextView textView = (TextView) findViewById(R.id.textView_Error);
+                        textView.setText("開始時刻が終了時刻より先にならないよう設定してください");
+                        break;
+                }
+                //検索結果画面へ移動
                 Intent intent = new Intent(DisasterSearchActivity.this, SearchResultListActivity.class);
+                //intent.putExtra(SEARCHCONDITIONS_DATA,search);
                 startActivity(intent);
+
 
             }
         });
@@ -167,6 +186,16 @@ public class DisasterSearchActivity extends AppCompatActivity {
         if(resultCode == RESULT_OK && requestCode == RESULT_AREA && intent != null){
             areaNum = intent.getIntExtra("areaNumber", 0);
         }
+    }
+
+    //検索範囲の開始日時が終了日時より未来の場合0を返す
+    protected int overTime(String startTime, String endTime){
+        int sTime = Integer.parseInt(startTime);
+        int eTime = Integer.parseInt(endTime);
+        if(sTime > eTime){
+            return 0;
+        }
+        return 1;
     }
 
 
--- app/src/main/java/com/example/idims/SearchConditions.java
+++ app/src/main/java/com/example/idims/SearchConditions.java
@@ -1,7 +1,9 @@
 package com.example.idims;
 
+import android.widget.SimpleAdapter;
+
 import java.util.Date;
-import java.util.Formatter;
+import java.util.Calendar;
 import java.text.SimpleDateFormat;
 
 //検索条件を格納するクラス
@@ -12,6 +14,9 @@ public class SearchConditions {
     public boolean thounder_On = false;
     //一定期間において全ての期間が選ばれているかを判定するbool型
     public boolean allConstant = false;
+    //日時を格納する変数
+    public String endDate;//最新の方
+    public String startDate;//過去の方
 
 
     //インストラクタ
@@ -19,50 +24,62 @@ public class SearchConditions {
 
     //期間設定において一定期間を選択した際にどの期間にするかを決定する
     public void settingConstant(String str) {
+        endDate = getToday();
         String all = "全て";
         String a_month = "過去１か月";
         String half_year = "過去半年";
         String a_year = "過去１年";
         String five_year = "過去５年";
         if (str.equals(all)) {
-            allConstant = true;
+            allConstant = true;//全ての日数有効化
         } else {
-            int today = getToday();
-            allConstant = false;
-            if (str.equals(a_month)) {//過去1か月を選択した場合
-                if()
+            allConstant = false;//全ての日数無効化
+            //検索の起点となる時刻(過去の時刻のこと)を算出する
+            Date date = new Date();//日数を格納
+            Calendar calendar = Calendar.getInstance();//日数計算のためにカレンダークラスを使用
+            calendar.setTime(date); //date型をcalendar型に変換
 
+            if (str.equals(a_month)) {//過去1か月を選択した場合
+                calendar.add(Calendar.MONTH, -1);
             } else if (str.equals(half_year)) {
+                calendar.add(Calendar.MONTH, -6);
             } else if (str.equals(a_year)) {
+                calendar.add(Calendar.YEAR, -1);
             } else if (str.equals(five_year)) {
+                calendar.add(Calendar.YEAR, -5);
             }
+            //計算された時刻をString型に変換する
+            startDate = getStringToday(calendar);
+
         }
     }
 
     //現在日時の取得
-    private int getToday(){
+    public String getToday(){
         Date date = new Date();
-        SimpleDateFormat today = new SimpleDateFormat("yyyy/MM/dd");
-        String str = today.format(date);//日付から文字列へ変換
-        return Integer.parseInt(str);//int型へ変換して返す
-
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd"); //年月日のみのデータに変換
+        String str = sdf.format(date); //文字型に変換
+        return str.replace("/", "");         //文字列にスラッシュ(//)が入っているので除去して返す
     }
-    //年の取得
-    private int getYear(int day){
 
+    //日時の型変換メソッド 上と処理が似ているがこちらは時刻の変換のみ
+    public String getStringToday(Calendar cal){
+        Date date = cal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd"); //年月日のみのデータに変換
+        String str = sdf.format(date);
+        return str.replace("/", "");
     }
-    //月の取得
-    private int getMonth(int day){
 
-    }
-    //日の取得
-    private int getDay(int day){
 
-    }
 
     //期間設定において自由期間を選択した際の期間選択
     public void settingFree(String startYear, String startMonth, String endYear, String endMonth){
-        allConstant = false;
+        allConstant = false; //一応「全て」の無効化
+        //文字列結合するだけ
+        //開始年月
+        startDate = new StringBuffer().append(startYear).append(startMonth).append("01").toString();
+        //終了年月
+        endDate = new StringBuffer().append(endYear).append(endMonth).append("01").toString();
 
     }
 
@@ -78,6 +95,14 @@ public class SearchConditions {
     //雷
     public void setThounder_On(boolean thounder_On){
         this.thounder_On = thounder_On;
+    }
+
+    public String getStartDate(){
+        return this.startDate;
+    }
+
+    public String getEndDate(){
+        return this.endDate;
     }
 
 }
