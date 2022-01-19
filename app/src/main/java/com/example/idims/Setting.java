8d2bfc7601641922365b663ea5b2582f0cfc4320
0120kan <eijirou0120@gmail.com>
2022-01-19 16:44:17 +0900

    設定画面　スイッチ動作　修正

--- app/src/main/java/com/example/idims/Setting.java
+++ app/src/main/java/com/example/idims/Setting.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 /*
     設定モジュール
@@ -17,8 +21,80 @@ public class Setting extends AppCompatActivity {
 
         //戻るボタン
         Button backButton = findViewById(R.id.backActivity);
-        backButton.setOnClickListener( v -> {
+        backButton.setOnClickListener(v -> {
             finish();
         });
+
+        //スイッチ、チェックボックス
+        Switch sw1 = findViewById(R.id.switch1);
+        Switch sw2 = findViewById(R.id.switch2);
+        Switch sw3 = findViewById(R.id.switch3);
+        CheckBox cb1 = findViewById(R.id.checkBox1);
+        CheckBox cb2 = findViewById(R.id.checkBox2);
+        CheckBox cb3 = findViewById(R.id.checkBox3);
+
+// スイッチ、チェックボックスをオンにする
+        sw1.setChecked(true);
+        sw2.setChecked(true);
+        sw3.setChecked(true);
+        sw3.setClickable(false);
+        cb1.setChecked(true);
+        cb2.setChecked(true);
+        cb3.setChecked(true);
+
+// イベント
+        // スイッチの状態が変化
+        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                if(sw1.isChecked()) {
+                    //スイッチがOFF->ONなら
+                    Toast.makeText(getApplication(), "位置情報ON", Toast.LENGTH_LONG).show();
+
+                    //ここに位置情報をONにするコードを入れたい
+
+
+                }else {
+                    //スイッチがON->OFFなら
+                    Toast.makeText(getApplication(), "位置情報OFF", Toast.LENGTH_LONG).show();
+
+                    //ここに位置情報をOFFにするコードを入れたい
+
+                }
+            }
+        });
+
+        sw2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                if(sw2.isChecked()) {
+                    //スイッチがOFF->ONなら
+                    Toast.makeText(getApplication(), "通知、アラートON", Toast.LENGTH_LONG).show();
+
+                    //ここに通知、アラートをONにするコードを入れたい
+                    sw3.setChecked(true);
+                    cb1.setChecked(true);
+                    cb2.setChecked(true);
+                    cb3.setChecked(true);
+                    cb1.setClickable(true);
+                    cb2.setClickable(true);
+                    cb3.setClickable(true);
+
+
+                }else {
+                    //スイッチがON->OFFなら
+                    Toast.makeText(getApplication(), "通知、アラートOFF", Toast.LENGTH_LONG).show();
+
+                    //ここに通知、アラートをOFFにするコードを入れたい
+                    sw3.setChecked(false);
+                    cb1.setChecked(false);
+                    cb2.setChecked(false);
+                    cb3.setChecked(false);
+                    cb1.setClickable(false);
+                    cb2.setClickable(false);
+                    cb3.setClickable(false);
+
+                }
+            }
+        });
+
     }
 }
