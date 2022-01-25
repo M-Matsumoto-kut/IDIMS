42bbf278293661e9472cdce61903e86a1c396915
hisui3 <hisui.mafgadama@gmail.com>
2022-01-26 00:19:21 +0900

    MenuActivity sakusei MenuActivity daitaisakusei

--- app/src/main/java/com/example/idims/Menu/MenuActivity.java
+++ app/src/main/java/com/example/idims/Menu/MenuActivity.java
@@ -2,9 +2,19 @@ package com.example.idims.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.idims.Area.AreaList;
+import com.example.idims.DisasterSearchActivity;
+import com.example.idims.HomeActivity;
+import com.example.idims.MainActivity;
+import com.example.idims.R;
 
 import com.example.idims.R;
+import com.example.idims.Researcher.ResearcherPage;
+import com.example.idims.Setting;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -12,6 +22,42 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //メニュー画面を閉じるボタン
+        Button closeMenu = findViewById(R.id.buttonToCloseMenuPage);
+        closeMenu.setOnClickListener( v -> {
+            Intent intent = new Intent(getApplication(), HomeActivity.class);
+            startActivity(intent);
+        });
+
+        //災害通知地域一覧へ遷移するボタン
+        Button TolistSetArea = findViewById(R.id.buttonToListSetAreaPage);
+        TolistSetArea.setOnClickListener( v -> {
+            Intent intent = new Intent(getApplication(), AreaList.class);
+            startActivity(intent);
+        });
+
+        //設定画面へ遷移するボタン
+        Button toSetting = findViewById(R.id.buttonToSettingPage);
+        toSetting.setOnClickListener( v -> {
+            Intent intent = new Intent(getApplication(), Setting.class);
+            startActivity(intent);
+        });
+
+        //災害情報検索画面へ遷移するボタン
+        Button toArchive = findViewById(R.id.buttonToArchivePage);
+        toArchive.setOnClickListener( v -> {
+            Intent intent = new Intent(getApplication(), DisasterSearchActivity.class);
+            startActivity(intent);
+        });
+
+        //研究者ページへ遷移するボタン
+        Button toResearcherPage = findViewById(R.id.buttonToResearcherPage);
+        toResearcherPage.setOnClickListener( v -> {
+            Intent intent = new Intent(getApplication(), ResearcherPage.class);
+            startActivity(intent);
+        });
+
 
     }
 }
